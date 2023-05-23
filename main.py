73d6c816fdf5cff77921265266cf9bf0dc926cf7
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-05-23 23:05:08 +0000

    palindromos

--- main.py
+++ main.py
@@ -2,5 +2,16 @@
 
 
 def is_palindrome(string: str) -> bool:
-    """Check if string is palindrome."""
-    return False
+    
+    string = ''.join(c.lower() for c in string if c.isalnum())
+
+    return string == string[::-1]
+
+testes_1 = ["", "a", "arara", "ola", "ovo", "mundo", "ame o poema", "Ame o poema!", "Hello, world!", "ola mundo", "Anotaram a data da maratona!"]
+
+for teste_2 in testes_1:
+    print(f">>> is_palindrome(\"{teste_2}\")")
+    print(is_palindrome(teste_2))
+    print()
+
+
